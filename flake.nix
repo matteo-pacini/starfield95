d5ed5259c42845762ba85c1408dca5fc14280965
Matteo Pacini <matteo.pacini@transreport.co.uk>
2025-02-07 09:38:11 +0000

    Static build

--- flake.nix
+++ flake.nix
@@ -16,9 +16,26 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pkgsStatic = nixpkgs.legacyPackages.${system}.pkgsStatic;
+        staticSDL2 =
+          (pkgsStatic.SDL2.override {
+            x11Support = !pkgsStatic.stdenv.hostPlatform.isLinux;
+          }).overrideAttrs
+            (old: {
+              postFixup = "";
+            });
         starfield95 = pkgs.callPackage ./starfield95.nix { };
+        staticStarfield = pkgsStatic.callPackage ./starfield95.nix {
+          SDL2 = staticSDL2;
+          pkg-config = pkgs.pkg-config;
+        };
       in
       {
+        packages = {
+          default = starfield95;
+          static = staticStarfield;
+        };
+
         apps.default = {
           type = "app";
           program = "${starfield95}/bin/starfield95";
--- starfield95.nix
+++ starfield95.nix
@@ -1,24 +1,17 @@
 {
   stdenv,
-  lib,
   SDL2,
-  SDL2_ttf,
   pkg-config,
-  dejavu_fonts,
 }:
 stdenv.mkDerivation {
   name = "starfield95";
   src = ./.;
+  nativeBuildInputs = [
+    pkg-config
+  ];
   buildInputs = [
     SDL2
-    SDL2_ttf
-    pkg-config
-    dejavu_fonts
   ];
-  preBuild = ''
-    substituteInPlace starfield95.c \
-      --replace "@fontPath@" "${dejavu_fonts}/share/fonts/truetype/DejaVuSans.ttf"
-  '';
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin
